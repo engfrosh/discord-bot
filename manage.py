56eb7f1b3e4f89891158ef8fa0d9d9a3e60b5635
grantmwilson <development@grantmwilson.com>
2022-07-05 19:04:47 -0400

    Added common models submodule

---
+++ manage.py
@@ -0,0 +1,18 @@
+from common_models.common_modules_setup import init_django
+
+DEFAULT_DATABASE = {
+    "ENGINE": "django.db.backends.postgresql_psycopg2",
+    "NAME": "engfrosh_dev_2022_07_01",
+    "USER": "engfrosh_bot",
+    "PASSWORD": "there-exercise-fenegle",
+    "HOST": "localhost",
+    "PORT": "5432",
+}
+
+INSTALLED_APPS = ['common_models', ]
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    init_django(installed_apps=INSTALLED_APPS, default_database=DEFAULT_DATABASE)
+    execute_from_command_line()
