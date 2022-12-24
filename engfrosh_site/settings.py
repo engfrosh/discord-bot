cf4c218021d14e0f521f5b988f82949b432cf49f
finlaywashere <finman292004@protonmail.com>
2022-12-23 20:22:39 -0500

    Got bot fully working with some tweaks

---
+++ engfrosh_site/settings.py
@@ -0,0 +1,3 @@
+# Discord API Settings
+DEFAULT_DISCORD_API_VERSION = 10
+DEFAULT_DISCORD_SCOPE = ["identify", "guilds.join"]
--- manage.py
+++
@@ -1,7 +0,0 @@
-from common_models.common_models_setup import init_django
-
-if __name__ == "__main__":
-    from django.core.management import execute_from_command_line
-
-    init_django()
-    execute_from_command_line()
